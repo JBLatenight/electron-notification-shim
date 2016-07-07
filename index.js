ce6bc93eb1855ec6824ffb3f58c411afe79f973b
Jeff Bargmann <jeffba@yahoo-inc.com>
2016-07-07 14:07:40 -0700

    Add Windows 7 support, via ability to save onclick handlers so main process may trigger

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 // Electron doesn't automatically show notifications in Windows yet, and it's not easy to polyfill.
 // So we have to hijack the Notification API.
-let ipc;
+let UUID = require('uuid'),
+	ipc;
 try {
 	// Using electron >=0.35
 	ipc = require('electron').ipcRenderer;
@@ -12,6 +13,7 @@ try {
 }
 
 var notificationShimController = {
+	saveOnClickHandlers: false, // Save notification onclick handlers so main thread may trigger them. (Windows 7 support.)
 	overrides: {
 		title: undefined,
 		body: undefined,
@@ -19,13 +21,13 @@ var notificationShimController = {
 		requireInteraction: undefined,
 		silent: undefined
 	}
-};
+}, savedNotifications = {};
 
 module.exports = () => {
 	const OldNotification = Notification;
 
 	Notification = function (title, options) {
-		var originalTitle, originalOptions, notification, i;
+		var originalTitle, originalOptions, uuid, notification, i;
 
 		// Apply overrides to the notification's options
 		originalTitle = title;
@@ -44,7 +46,9 @@ module.exports = () => {
 		// Send this to main thread.
 		// Catch it in your main 'app' instance with `ipc.on`.
 		// Then send it back to the view, if you want, with `event.returnValue` or `event.sender.send()`.
+		uuid = (notificationShimController.saveOnClickHandlers ? UUID.v4() : null);
 		ipc.send('notification-shim', {
+			uuid,
 			title,
 			options,
 			originalTitle,
@@ -58,6 +62,9 @@ module.exports = () => {
 			}
 			var onclickOld = notification.onclick;
 			notification.onclick = function () {
+				if (uuid && savedNotifications[uuid]) {
+					delete savedNotifications[uuid];
+				}
 				ipc.send('notification-shim-onclick', notification);
 				if (onclickOld) {
 					onclickOld();
@@ -67,12 +74,41 @@ module.exports = () => {
 
 		// Send the native Notification.
 		// You can't catch it, that's why we're doing all of this. :)
-		return notification = new OldNotification(title, options);
+		notification = new OldNotification(title, options);
+
+		// Save Notification objects so main thread can invoke their onclick functions
+		if (notificationShimController.saveOnClickHandlers) {
+			if (notification && uuid) {
+				savedNotifications[uuid] = notification;
+			}
+		}
+
+		// Return browser-instantiated Notification
+		return notification;
 	};
 
 	Notification.prototype = OldNotification.prototype;
 	Notification.permission = OldNotification.permission;
 	Notification.requestPermission = OldNotification.requestPermission;
+
+	// Allow main thread to signal dismissed and/or clicked. (Windows 7 support)
+	ipc.on('notification-shim-notification-dismissed', function handleNotificationShimResponse(sender, uuid, clicked) {
+		if (!uuid) {
+			return;
+		}
+
+		// Look up from (and remove from) saved handlers
+		var notification = savedNotifications[uuid];
+		if (!notification) {
+			return;
+		}
+		delete savedNotifications[uuid];
+
+		// Call handler
+		if (clicked && notification.onclick) {
+			notification.onclick();
+		}
+	});
 
 	return notificationShimController;
 };
